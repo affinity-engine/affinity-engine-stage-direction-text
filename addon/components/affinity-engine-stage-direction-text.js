b19c20cbf899a4212aaf6f9662c0cb8790de374b
null null null <null@revolution.cheap>
2016-07-01 16:32:16 -0700

    add component

---
+++ addon/components/affinity-engine-stage-direction-text.js
@@ -0,0 +1,90 @@
+import Ember from 'ember';
+import layout from '../templates/components/affinity-engine-stage-direction-text';
+import { configurable, deepConfigurable } from 'affinity-engine';
+import { DirectableComponentMixin, StyleableComponentMixin, TransitionableComponentMixin } from 'affinity-engine-stage';
+import multiton from 'ember-multiton-service';
+
+const {
+  Component,
+  computed,
+  get,
+  isPresent
+} = Ember;
+
+const { inject: { service } } = Ember;
+const { alias } = computed;
+
+const configurationTiers = [
+  'directable.attrs',
+  'character.attrs.text',
+  'character.attrs',
+  'character.attrs.fixture.text',
+  'character.attrs.fixture',
+  'config.attrs.director.text',
+  'config.attrs.director',
+  'config.attrs.globals'
+];
+
+export default Component.extend(DirectableComponentMixin, StyleableComponentMixin, TransitionableComponentMixin, {
+  layout,
+
+  classNames: ['et-text-container'],
+
+  config: multiton('affinity-engine/config', 'engineId'),
+  translator: service('affinity-engine/translator'),
+
+  character: alias('directable.attrs.character'),
+
+  customClassNames: configurable(configurationTiers, 'classNames'),
+  cps: configurable(configurationTiers, 'cps'),
+  keyboardPriority: configurable(configurationTiers, 'keyboardPriority'),
+  keys: configurable(configurationTiers, 'keys.accept'),
+  instant: configurable(configurationTiers, 'instant'),
+  name: configurable(configurationTiers, 'name'),
+  namePosition: configurable(configurationTiers, 'namePosition'),
+  scrollable: configurable(configurationTiers, 'scrollable'),
+  transitionIn: deepConfigurable(configurationTiers, 'transitionIn', 'transition'),
+  transitionOut: deepConfigurable(configurationTiers, 'transitionOut'),
+  text: configurable(configurationTiers, 'text'),
+  tweenEffect: configurable(configurationTiers, 'tweenEffect'),
+  tweenRate: configurable(configurationTiers, 'tweenRate'),
+
+  didInsertElement(...args) {
+    this._super(...args);
+
+    this._handlePriorSceneRecord();
+    this._transitionInText();
+  },
+
+  _handlePriorSceneRecord() {
+    if (isPresent(get(this, 'priorSceneRecord'))) {
+      this.resolveAndDestroy();
+    }
+  },
+
+  _transitionInText() {
+    this.executeTransitionIn();
+  },
+
+  nameTranslation: computed('name', {
+    get() {
+      return get(this, 'translator').translate(get(this, 'name'));
+    }
+  }).readOnly(),
+
+  textTranslation: computed('text', {
+    get() {
+      return get(this, 'translator').translate(get(this, 'text'));
+    }
+  }).readOnly(),
+
+  actions: {
+    completeText() {
+      if (get(this, 'willResolveExternally')) { return; }
+
+      this.executeTransitionOut().then(() => {
+        this.resolveAndDestroy();
+      });
+    }
+  }
+});
---
+++ app/components/affinity-engine-stage-direction-text.js
@@ -0,0 +1,1 @@
+export { default } from 'affinity-engine-stage-direction-text/components/affinity-engine-stage-direction-text';
