421446441a7054a0d93593018a6d16919313d48f
null null null <null@affinitygamescollective.org>
2016-08-04 20:21:51 -0700

    update translator

--- addon/components/affinity-engine-stage-direction-text.js
+++ addon/components/affinity-engine-stage-direction-text.js
@@ -71,13 +71,19 @@ export default Component.extend(DirectableComponentMixin, StyleableComponentMixi
 
   nameTranslation: computed('name', {
     get() {
-      return get(this, 'translator').translate(get(this, 'name'));
+      const name = get(this, 'name.text') || get(this, 'name');
+      const options = get(this, 'name.options');
+
+      return get(this, 'translator').translate(name, options) || name;
     }
   }).readOnly(),
 
   textTranslation: computed('text', {
     get() {
-      return get(this, 'translator').translate(get(this, 'text'));
+      const text = get(this, 'text.text') || get(this, 'text');
+      const options = get(this, 'text.options');
+
+      return get(this, 'translator').translate(text, options) || text;
     }
   }).readOnly(),
 
